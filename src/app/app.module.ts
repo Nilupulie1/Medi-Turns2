f810ba1a583107d2c3f51b3d958e2b3232571a8c
Nilupulie Kariyawasam <nilupuliekariyawasam42@gmail.com>
2018-04-20 16:33:16 +0530

    Create Home, about us and terms and conditions

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,21 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { HomeComponent } from './home/home.component';
+import { AboutUsComponent } from './about-us/about-us.component';
+import { TermsComponent } from './terms/terms.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    HomeComponent,
+    AboutUsComponent,
+    TermsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule

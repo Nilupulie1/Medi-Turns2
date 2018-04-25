339dfa611db54ebec01ffe4454f9e57953598631
Nilupulie Kariyawasam <nilupuliekariyawasam42@gmail.com>
2018-04-25 16:39:57 +0530

    add about us and facilities

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,23 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { BrandNavComponent } from './brand-nav/brand-nav.component';
+import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { FacilitiesComponent } from './facilities/facilities.component';
+import { TermsComponent } from './terms/terms.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrandNavComponent,
+    TopNavBarComponent,
+    HomeComponent,
+    AboutComponent,
+    FacilitiesComponent,
+    TermsComponent
   ],
   imports: [
     BrowserModule

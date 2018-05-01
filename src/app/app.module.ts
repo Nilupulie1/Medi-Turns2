4c452088a0ac4c88951ee77cc8590bbabc4c3ca3
Nilupulie Kariyawasam <nilupuliekariyawasam42@gmail.com>
2018-05-01 15:21:25 +0530

    1st page before finals

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { BrandNavComponent } from './brand-nav/brand-nav.component';
+import { TopNavBarComponent } from './top-nav-bar/top-nav-bar.component';
+import { HomeComponent } from './home/home.component';
+import { AboutComponent } from './about/about.component';
+import { FacilitiesComponent } from './facilities/facilities.component';
+import { TermsComponent } from './terms/terms.component';
+import { SpecialistsComponent } from './specialists/specialists.component';
+import { ContactComponent } from './contact/contact.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BrandNavComponent,
+    TopNavBarComponent,
+    HomeComponent,
+    AboutComponent,
+    FacilitiesComponent,
+    TermsComponent,
+    SpecialistsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/brand-nav/brand-nav.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-brand-nav',
+  templateUrl: './brand-nav.component.html',
+  styleUrls: ['./brand-nav.component.css']
+})
+export class BrandNavComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
